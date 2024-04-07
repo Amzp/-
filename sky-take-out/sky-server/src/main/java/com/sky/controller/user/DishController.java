d590fe76e57d23864ede0c35ec66132cb23432a6
Rain^ <843524258@qq.com>
2024-04-07 11:10:10 +0800

    Day07: redis缓存菜品数据

--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,9 +9,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController("userDishController")
@@ -22,6 +24,9 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     /**
      * 根据分类id查询菜品
      *
@@ -31,13 +36,26 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
     public Result<List<DishVO>> list(Long categoryId) {
+        // 构造redis中的key，规则：dish_categoryId
+        String key = "dish_" + categoryId;
+
+        // 查询redis中是否存在菜品数据
+        List<DishVO> redisList = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if (redisList != null && !redisList.isEmpty()) {
+            // 如果存在，则直接返回，无需再次查询数据库
+            log.info("从redis中获取数据：categoryId = {}", categoryId);
+            return Result.success(redisList);
+        }
+
+        // 如果不存在，则查询数据库，并将数据存入redis
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
 
-        List<DishVO> list = dishService.listWithFlavor(dish);
+        List<DishVO> sqlList = dishService.listWithFlavor(dish); // 查询数据库中的菜品数据
+        log.info("从数据库中获取数据：categoryId = {}", categoryId);
+        redisTemplate.opsForValue().set(key, sqlList); // 将数据存入redis
 
-        return Result.success(list);
+        return Result.success(sqlList);
     }
-
 }
