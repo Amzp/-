cd5e0b974cd17f7520f3f77e8feecedeb613990f
Rain^ <843524258@qq.com>
2024-04-09 20:46:26 +0800

    Day12：工作台

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -82,4 +83,12 @@ public interface DishMapper {
             "where id = #{id}")
     @AutoFill(value = OperationType.UPDATE) // 公共字段填充（updateTime,updateUser）
     void updateSaleStatus(Dish dish);
+
+    /**
+     * 根据条件统计菜品数量
+     *
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
--- sky-take-out/sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -5,7 +5,6 @@ import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
-import com.sky.result.PageResult;
 import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Mapper;
@@ -13,6 +12,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -111,4 +111,12 @@ public interface SetmealMapper {
             "from sky_take_out.setmeal_dish sd left join sky_take_out.dish d on sd.dish_id = d.id " +
             "where sd.setmeal_id = #{setmealId}")
     List<DishItemVO> getDishItemBySetmealId(Long setmealId);
+
+    /**
+     * 根据条件统计套餐数量
+     *
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
