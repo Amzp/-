37b414507450eca1482f99eeee975d5af82b669b
Rain^ <843524258@qq.com>
2024-04-09 17:12:26 +0800

    Day11：用户数量统计

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 /**
  * ClassName: UserMapper
  * Package: com.sky.mapper
@@ -31,4 +33,6 @@ public interface UserMapper {
 
     @Select("select * from sky_take_out.user where id = #{userId}")
     User getById(Long userId);
+
+    Integer countByMap(Map map);
 }
